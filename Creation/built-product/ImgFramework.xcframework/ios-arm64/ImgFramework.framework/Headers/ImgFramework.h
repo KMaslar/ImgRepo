1cda6121cf53fd9a1ce695dd93749193a9fedf9f
Kostadina Geceska <kostadinag@nextsense.com>
2024-07-04 11:18:34 +0200

    initial

---
+++ Creation/built-product/ImgFramework.xcframework/ios-arm64/ImgFramework.framework/Headers/ImgFramework.h
@@ -0,0 +1,18 @@
+//
+//  ImgFramework.h
+//  ImgFramework
+//
+//  Created by Kostadina Gecevska on 4.7.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImgFramework.
+FOUNDATION_EXPORT double ImgFrameworkVersionNumber;
+
+//! Project version string for ImgFramework.
+FOUNDATION_EXPORT const unsigned char ImgFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImgFramework/PublicHeader.h>
+
+
